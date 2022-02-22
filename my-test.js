8f4935cfdc77e35c34ac42f32fc2e7b10c9baf7b
Kybik <82117054+kybikn@users.noreply.github.com>
2022-02-22 20:19:22 +0300

    feat: task done

---
+++ my-test.js
@@ -0,0 +1,7 @@
+const towelSort = require("./src/index.js");
+const matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+];
+console.log(towelSort(matrix));
--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
-
-// You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    let result = [];
+    if (!matrix) {
+        return result;
+    }
+    for (let i = 0; i < matrix.length; i++) {
+        if (i % 2 !== 0) {
+            matrix[i].reverse();
+            result = result.concat(matrix[i]);
+        } else {
+            result = result.concat(matrix[i]);
+        }
+    }
+    return result;
+};
